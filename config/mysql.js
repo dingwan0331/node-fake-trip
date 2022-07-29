1fb219ce65d1c9863fa211a9a375f07d4bf4f77c
dingwan <dingwan0331@gmail.com>
2022-07-29 13:21:16 +0900

    initial settings

---
+++ config/mysql.js
@@ -0,0 +1,18 @@
+const dotenv = require("dotenv")
+const { DataSource } = require('typeorm');
+
+dotenv.config()
+
+const myDataSource = new DataSource({
+    type     : process.env.TYPEORM_CONNECTION,
+    host     : process.env.TYPEORM_HOST,
+    port     : process.env.TYPEORM_PORT,
+    username : process.env.TYPEORM_USERNAME,
+    password : process.env.TYPEORM_PASSWORD,
+    database : process.env.TYPEORM_DATABASE
+})
+
+module.exports = myDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
